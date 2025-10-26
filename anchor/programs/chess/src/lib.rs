36454da437290e01c5656830d7684ebd1dca290b
Danish Ansari <dvansari360@gmail.com>
2025-10-26 19:06:38 +0530

    fix bugs

--- anchor/programs/chess/src/lib.rs
+++ anchor/programs/chess/src/lib.rs
@@ -7,7 +7,7 @@ pub mod anchor {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        msg!("Greetings from: {:?}" , ctx.program_id);
         Ok(())
     }
 }
