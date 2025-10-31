1c3d714604378fd7151fbc0c9bcd2bfd5cfd7ced
Danish Ansari <dvansari360@gmail.com>
2025-10-31 18:07:24 +0530

    created react query provider

---
+++ web/components/react-query-provider.tsx
@@ -0,0 +1,32 @@
+import { isServer, QueryClient, QueryClientProvider, useQueryClient } from "@tanstack/react-query"
+
+
+function makeQueryClient (){
+   return new QueryClient({
+    defaultOptions:{
+        queries:{
+            staleTime:60*1000
+        }
+    }
+   })
+}
+let browseQueryClient : QueryClient | undefined;
+function getQueryClient (){
+    if(isServer){
+        return makeQueryClient()
+    }else{
+        if(!browseQueryClient) browseQueryClient = makeQueryClient()
+        return browseQueryClient;
+    }
+}
+
+function ReactQueryProvider ({children}:{children:React.ReactNode}){
+    const queryClient = getQueryClient()
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+}
+
+export default ReactQueryProvider
