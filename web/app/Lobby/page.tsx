f065d75021dc3d00fb8afe3783d9c68d5dcb96aa
Danish Ansari <dvansari360@gmail.com>
2025-11-01 20:20:27 +0530

    updated lobby page

--- web/app/Lobby/page.tsx
+++ web/app/Lobby/page.tsx
@@ -1,11 +1,40 @@
-import LobbyHeader from '@/components/lobby/header'
-import React from 'react'
+"use client";
+import LobbyHeader from "@/components/lobby/header";
+import UserSidebar from "@/components/lobby/user-sidebar";
+import { player } from "@/types/player";
+import React, { useEffect, useState } from "react";
 
 export default function Lobby() {
-  return (
-    <div className='w-full h-screen '>
-        <LobbyHeader/>
-    </div>
-  )
-}
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+  const openUserProfile = () => setIsSidebarOpen(true);
+  const closeUserProfile = () => setIsSidebarOpen(false);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const mockUser: player = {
+    userName: "Danish",
+    lost: 3,
+    wins: 10,
+    createdAt: "2025-01-01T00:00:00Z",
+    rating: 1420,
+    solWon: 5,
+  };
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <div className="w-full h-full ">
+      <div>
+        <LobbyHeader openUserProfile={openUserProfile} openFilter={() => {}} />
+        <UserSidebar
+          isOpen={isSidebarOpen}
+          onClose={closeUserProfile}
+          user={mockUser}
+        />
+      </div>
+    </div>
+  );
+}
