cf0a486eac5fc69a7d2fffbab32e647c96125d25
Danish Ansari <dvansari360@gmail.com>
2025-11-01 13:34:26 +0530

    added types for player

---
+++ web/types/player.ts
@@ -0,0 +1,8 @@
+export interface player {
+    userName:string  | undefined,
+    lost:number | null;
+    wins:number | null;
+    createdAt:string,
+    rating:number | null,
+    solWon:number | null
+}
