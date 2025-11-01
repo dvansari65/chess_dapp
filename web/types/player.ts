e5c0a7e33257b356160158f14f5fed4c1ec8f40b
Danish Ansari <dvansari360@gmail.com>
2025-11-01 20:19:20 +0530

    updated types of player

--- web/types/player.ts
+++ web/types/player.ts
@@ -1,8 +1,14 @@
 export interface player {
-    userName:string  | undefined,
-    lost:number | null;
-    wins:number | null;
-    createdAt:string,
-    rating:number | null,
-    solWon:number | null
+  id: number | undefined;
+  avatar: string | undefined;
+  userName: string | undefined;
+  lost: number | null;
+  wins: number | null;
+  createdAt: string;
+  rating: number | null;
+  solWon: number | null;
+  status : "online" | "offline",
+  isPlaying:boolean,
+  matchesPlayed:number | null
 }
+
