ff1967822d160e798517be019028bedd85d07bf3
Danish Ansari <dvansari360@gmail.com>
2025-11-08 16:43:05 +0530

    created skeleton laoder

---
+++ web/components/Loader/oppenent-loader.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function OpponentsLoader() {
+  return (
+    <div
+      className="flex gap-4 text-stone-100 px-6 py-6 m-2 rounded-2xl
+      bg-gradient-to-br from-white/15 to-white/5 backdrop-blur-xl
+      border border-white/20 shadow-[0_0_20px_rgba(255,255,255,0.05)]
+      animate-pulse"
+    >
+      {/* Loading content skeletons can go here */}
+    </div>
+  );
+}
+
+export default OpponentsLoader;
