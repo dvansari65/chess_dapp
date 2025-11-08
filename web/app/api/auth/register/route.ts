c1e6b2b9e394f8409ae9410495132208fd2f2a49
Danish Ansari <dvansari360@gmail.com>
2025-11-08 10:49:55 +0530

    created user register api

--- web/app/api/auth/register/route.ts
+++ web/app/api/auth/register/route.ts
@@ -1,11 +1,38 @@
-import { NextRequest } from "next/server";
+
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
 
 
 export const POST = async(req:NextRequest)=>{
     try {
         const body = await req.json()
-        const {username,p} = body
-    } catch (error) {
-        
+        const {userName,publickey} = body
+        if(!userName){
+            throw new Error("Please provide user name!")
+        }
+        if(!publickey){
+            throw new Error("Please connect your wallet first!")
+        }
+        const user = await prisma.player.create({
+            data:{
+                userName:userName as string,
+                publickey:publickey as string,
+                createdAt:String(new Date())
+            }
+        })
+        if(!user){
+            throw new Error("failed to generate user!")
+        }
+        return NextResponse.json({
+            message:"user created successfully!",
+            user
+        })
+    } catch (error:any) {
+        return NextResponse.json(
+            {
+                error:error.message || "server error!"
+            },
+            {status:500}
+        )
     }
 }
