264d28ccfa31876ec0fe4249391cf6301945bc31
Danish Ansari <dvansari360@gmail.com>
2025-11-08 15:30:36 +0530

    created api to get all players

---
+++ web/app/api/user/route.ts
@@ -0,0 +1,42 @@
+import prisma from "@/lib/prisma"
+import { NextResponse } from "next/server"
+
+export const GET = async()=>{
+    try {
+        const users = await prisma.player.findMany({})
+        if(!users || users.length === 0){
+            return NextResponse.json(
+                {
+                    message:"No users found!",
+                    users:[],
+                    success:false
+                },
+                {
+                    status:404
+                }
+            )
+        }
+        return NextResponse.json(
+            {
+                message:"Users found successfully!",
+                users,
+                success:true
+            },
+            {
+                status:200
+            }
+        )
+    } catch (error:any) {
+        console.log(error)
+        return NextResponse.json(
+            {
+                error:error.message,
+                success:false,
+                user:[]
+            },
+            {
+                status:200
+            }
+        )
+    }
+}
