fe65d43ad9754e7d7cb40fbd38dfbaef296d3d6d
Danish Ansari <dvansari360@gmail.com>
2025-11-08 12:10:19 +0530

    created api to register user

--- web/apis/register.ts
+++ web/apis/register.ts
@@ -4,16 +4,17 @@ import { useMutation } from "@tanstack/react-query"
 export const Register = ()=>{
     return useMutation({
         mutationKey:["register"],
-        mutationFn:async({userName,avatar}:{userName:string,avatar:string})=>{
+        mutationFn:async({userName,publickey}:{userName:string,publickey:string})=>{
             try {
-                const response = await fetch("/api/register",{
+                const response = await fetch("/api/auth/register",{
                     method:"POST",
-                    body:JSON.stringify({userName,avatar}),
+                    body:JSON.stringify({userName,publickey}),
                     headers:{
                         "Content-Type":"application/json"
                     }
                 })
                 const data = await response.json()
+                console.log("register user data",data)
                 if(!response.ok){
                     throw new Error(data.message || "something went wrong!")
                 }
