129d0048892ff6cbd0404b9216c08b131e19c2b9
Danish Ansari <dvansari360@gmail.com>
2025-11-08 15:47:21 +0530

    created api to fetch all users at the frontend

---
+++ web/apis/getAllPlayers.ts
@@ -0,0 +1,23 @@
+
+import { getAllPlayersResponse, player } from "@/types/player"
+import { useQuery } from "@tanstack/react-query"
+
+
+export const getAllPlayers = ()=>{
+    return useQuery<getAllPlayersResponse>({
+        queryKey:["users"],
+        queryFn:async()=>{
+            try {
+                const response = await fetch("/api/user")
+                const data = await response.json()
+                if(!response.ok){
+                    throw new Error(data.error || "something went wrong!")
+                }
+                console.log("users",data.users)
+                return data;
+            } catch (error) {
+                throw error
+            }
+        }
+    })
+}
