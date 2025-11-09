c8e91957910a684adb7fdc9b3b60d10fc901efe3
Danish Ansari <dvansari360@gmail.com>
2025-11-09 22:33:17 +0530

    updated

--- web/app/Lobby/page.tsx
+++ web/app/Lobby/page.tsx
@@ -6,29 +6,48 @@ import LobbyHeader from "@/components/lobby/header";
 import UserSidebar from "@/components/lobby/user-sidebar";
 import Oppenent from "@/components/opponent";
 import { useWallet } from "@solana/wallet-adapter-react";
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { useSocket } from "../hooks/useSocket";
+import { toast } from "react-toastify";
 
 export default function Lobby() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
- const {publicKey} = useWallet()
- const { data:UserData,refetch,isPending:isUserLoading } = getPlayer(publicKey);
+  const { publicKey } = useWallet();
+  const {
+    data: UserData,
+    refetch,
+    isPending: isUserLoading,
+  } = getPlayer(publicKey);
   const openUserProfile = () => setIsSidebarOpen(true);
   const closeUserProfile = () => setIsSidebarOpen(false);
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+  const { socket, isConnected } = useSocket();
   const { data, isPending, error } = getAllPlayers();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    if (!socket || !isConnected) {
+      toast.error("socket not connected!");
+      return;
+    }
+  }, [socket, data]);
 
   return (
     <div className="w-full h-full ">
       <div>
         <LobbyHeader openUserProfile={openUserProfile} openFilter={() => {}} />
-        <UserSidebar user={UserData?.user} isOpen={isSidebarOpen} onClose={closeUserProfile} isLoading={isUserLoading} />
+        <UserSidebar
+          user={UserData?.user}
+          isOpen={isSidebarOpen}
+          onClose={closeUserProfile}
+          isLoading={isUserLoading}
+        />
       </div>
       <div>
         {isPending &&
@@ -39,12 +58,12 @@ export default function Lobby() {
           ))}
         {data?.users?.map((user) => (
           <div key={user.id}>
-            <Oppenent 
+            <Oppenent
               userName={user.userName}
               publickey={user.publickey}
               status={user.status}
-              challenge={()=>{}}
-              ratings={user.rating }
+              challenge={() => {}}
+              ratings={user.rating}
               currentPlayer={publicKey}
             />
           </div>
