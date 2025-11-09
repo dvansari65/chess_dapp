fb973b43a87b1dd7187df6d93775cb9bb328b7c4
Danish Ansari <dvansari360@gmail.com>
2025-11-09 09:10:44 +0530

    updated

--- web/components/Loader/oppenent-loader.tsx
+++ web/components/Loader/oppenent-loader.tsx
@@ -4,8 +4,8 @@ function OpponentsLoader() {
   return (
     <div
       className="flex gap-4 text-stone-100 px-6 py-6 m-2 rounded-2xl
-      bg-gradient-to-br from-white/15 to-white/5 backdrop-blur-xl
-      border border-white/20 shadow-[0_0_20px_rgba(255,255,255,0.05)]
+      bg-gradient-to-br from-black to-white/5 backdrop-blur-xl
+      border border-white/20 
       animate-pulse"
     >
       {/* Loading content skeletons can go here */}
