58591d107ceafd92ddda780c81d0931e6ffd95d6
Danish Ansari <dvansari360@gmail.com>
2025-11-16 12:02:18 +0530

    added tests for game counter

--- anchor/tests/anchor.ts
+++ anchor/tests/anchor.ts
@@ -1,24 +1,55 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import {Chess} from "../target/types/chess"
+import { assert } from "chai";
 import { BN } from "bn.js";
 
 describe("anchor", () => {
   // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.anchor as Program<Chess>;
+  const program = anchor.workspace.chess as Program<Chess>;
+  const [gameIdPda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("counter"),
+    ],
+    program.programId
+  )
 
   it("Is initialized!", async () => {
-    const gameId = new BN(112823);
-    const [gamePda] = anchor.web3.PublicKey.findProgramAddressSync(
-      [gameId.toArrayLike(Buffer,"le",8)],
-      program.programId
-    )
+    console.log("test started!")
+   
+   try {
+     const tx = await program.methods
+     .initializeGameId()
+     .rpc()
+     const account = await program.account.counter.fetch(gameIdPda)
+    console.log("tx",tx)
+    console.log("Account",account.gameId)
+    assert(account.gameId.eq(new BN(0)))
+   } catch (error) {
+      throw error;
+   }
+  });
+  it("create game initialized",async()=>{
+    console.log("Create game initialize.....")
+    const wagerAmount = new BN(1_000_000_000);
+    const counterAccount = await program.account.counter.fetch(gameIdPda)
+    const gameId = counterAccount.gameId.toNumber()
+    const nextGameId = gameId + 1;
     try {
-      const account = await program.account.game.fetch(gamePda)
+      const tx = await program.methods
+      .createGame(wagerAmount)
+      .accounts({
+        player1:provider.wallet.publicKey
+      })
+      .rpc()
+      const createGameAcount = await program.account.counter.fetch(gameIdPda)
+      console.log("Updated game counter:", createGameAcount.gameId.toString());
+      assert(createGameAcount.gameId.eq(new BN(nextGameId)))
     } catch (error) {
       
     }
-  });
+  })
 });
