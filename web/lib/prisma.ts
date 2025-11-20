8e61560c862f849363b1a0f11f5118c8b11d0ea8
Danish Ansari <dvansari360@gmail.com>
2025-11-20 17:18:23 +0530

    updated prisma client wit prisma 7 update

--- web/lib/prisma.ts
+++ web/lib/prisma.ts
@@ -1,14 +1,28 @@
-import { PrismaClient } from "@/generated/client"
+import { PrismaClient } from "@/generated/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import {config} from "dotenv"
 
+config()
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma: PrismaClient | undefined;
+};
+
+const connectionString = process.env.DATABASE_URL;
+console.log("DATABASE_URL:", connectionString);
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set!");
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  log: ['query', 'error', 'warn'],
-})
+const adapter = new PrismaPg({ connectionString});
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter, 
+  });
 
-export default prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
