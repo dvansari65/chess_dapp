ea15ca7aa27ae1879f394cc772bd236bb6dcec1b
Danish Ansari <dvansari360@gmail.com>
2025-11-21 14:55:00 +0530

    created global event

--- web/utils/socketProvider.tsx
+++ web/utils/socketProvider.tsx
@@ -1,19 +1,43 @@
 "use client";
 
-import { createContext, useContext, useEffect, ReactNode, useMemo } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  ReactNode,
+  useMemo,
+} from "react";
 import { initializeSocket } from "@/lib/socket";
-import  { Socket } from "socket.io-client";
+import { Socket } from "socket.io-client";
+import { ChallengeStatus } from "@/generated/enums";
+import { toast } from "react-toastify";
 
 interface SocketProviderProps {
   children: ReactNode;
+  currentPlayerPubKey: string | undefined;
 }
 
 const SocketContext = createContext<Socket | null>(null);
 
-export const SocketProvider = ({ children }: SocketProviderProps) => {
+export const SocketProvider = ({
+  children,
+  currentPlayerPubKey,
+}: SocketProviderProps) => {
+
   const socket = useMemo(() => initializeSocket(), []);
   useEffect(() => {
     socket.connect();
+
+    const handlerRejectedChallenge = (data: {
+      currentPlayerPubKey: string | undefined;
+      challengeStatus: ChallengeStatus;
+    }) => {
+      if (data.currentPlayerPubKey === currentPlayerPubKey) {
+        toast.info("Your challenge was rejected!");
+      }
+    };
+
+    socket.on("challenge-rejected",handlerRejectedChallenge)
 
     socket.on("connect", () => {
       console.log("✅ Socket connected:", socket.id);
@@ -29,9 +53,7 @@ export const SocketProvider = ({ children }: SocketProviderProps) => {
   }, [socket]);
 
   return (
-    <SocketContext.Provider value={socket}>
-      {children}
-    </SocketContext.Provider>
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
   );
 };
 
